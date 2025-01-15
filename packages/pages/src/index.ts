2cbb9af6c37b339201725c5d2b05259ae12ff4a6
reaper <ahoy@barelyhuman.dev>
2025-01-15 12:44:28 +0530

    fix: add route types

--- packages/pages/src/index.ts
+++ packages/pages/src/index.ts
@@ -6,37 +6,43 @@ import type { Plugin } from "vite";
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+export type RouteDefinitions<T> = {
+	route: string;
+	routePath: string;
+	module: () => Promise<T>;
+};
+
 export default function routes({
-  root = "/src/pages",
-  id: virtualId = "~routes",
-  extensions = ["js", "ts", "tsx", "jsx"],
-  replacer = "",
+	root = "/src/pages",
+	id: virtualId = "~routes",
+	extensions = ["js", "ts", "tsx", "jsx"],
+	replacer = "",
 } = {}): Plugin {
-  return {
-    name: "barelyhuman-pages",
-    enforce: "pre",
-    resolveId(id) {
-      if (id !== virtualId) {
-        return;
-      }
-      return `/0${virtualId}`;
-    },
-    async load(id) {
-      if (id !== `/0${virtualId}`) {
-        return;
-      }
+	return {
+		name: "barelyhuman-pages",
+		enforce: "pre",
+		resolveId(id) {
+			if (id !== virtualId) {
+				return;
+			}
+			return `/0${virtualId}`;
+		},
+		async load(id) {
+			if (id !== `/0${virtualId}`) {
+				return;
+			}
 
-      const extsString = extensions.join(",");
-      const code = (
-        await readFile(join(__dirname, "./runtime/pages.js"), "utf8")
-      )
-        .replace(/\#\{__PLUGIN_PAGES_ROOT\}/g, `${root}/**/*.{${extsString}}`)
-        .replace(/\#\{__PLUGIN_PAGES_ROOT_REGEX\}/, `^${root}`)
-        .replace(/\#\{__PLUGIN_PAGES_ROOT_REGEX_REPLACER\}/, replacer);
+			const extsString = extensions.join(",");
+			const code = (
+				await readFile(join(__dirname, "./runtime/pages.js"), "utf8")
+			)
+				.replace(/\#\{__PLUGIN_PAGES_ROOT\}/g, `${root}/**/*.{${extsString}}`)
+				.replace(/\#\{__PLUGIN_PAGES_ROOT_REGEX\}/, `^${root}`)
+				.replace(/\#\{__PLUGIN_PAGES_ROOT_REGEX_REPLACER\}/, replacer);
 
-      return {
-        code,
-      };
-    },
-  };
+			return {
+				code,
+			};
+		},
+	};
 }
