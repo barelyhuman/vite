f06d49957ced7f2a48ac05294eb753c08df185ca
reaper <ahoy@barelyhuman.dev>
2025-01-26 00:40:01 +0530

    feat: use chunked globs instead of a single string

--- packages/pages/src/index.ts
+++ packages/pages/src/index.ts
@@ -1,5 +1,5 @@
-import { readFile } from "node:fs/promises";
-import { dirname, join } from "node:path";
+import { readFile, readdir, stat } from "node:fs/promises";
+import { dirname, extname, join } from "node:path";
 import { fileURLToPath } from "node:url";
 
 import type { Plugin } from "vite";
@@ -17,10 +17,16 @@ export default function routes({
 	id: virtualId = "~routes",
 	extensions = ["js", "ts", "tsx", "jsx"],
 	replacer = "",
+	ignoredPath = "",
+	isExcluded = (filepath: string) => false,
 } = {}): Plugin {
+	let cfg: { root: string };
 	return {
 		name: "barelyhuman-pages",
 		enforce: "pre",
+		configResolved(_cfg) {
+			cfg = _cfg;
+		},
 		resolveId(id) {
 			if (id !== virtualId) {
 				return;
@@ -32,11 +38,19 @@ export default function routes({
 				return;
 			}
 
-			const extsString = extensions.join(",");
+			const projectRoot = join(cfg.root, root);
+			const matches = await readdirRecursive(projectRoot);
+
+			const usableMatches = matches
+				.filter((d) => extensions.includes(extname(d)))
+				.filter((d) => !isExcluded(d.replace(projectRoot, "")))
+				.map((d) => d.replace(projectRoot, ""));
+
+			const globRegex = `${root}{${usableMatches.join(",")}}`;
 			const code = (
 				await readFile(join(__dirname, "./runtime/pages.js"), "utf8")
 			)
-				.replace(/\#\{__PLUGIN_PAGES_ROOT\}/g, `${root}/**/*.{${extsString}}`)
+				.replace(/\#\{__PLUGIN_PAGES_ROOT\}/g, globRegex)
 				.replace(/\#\{__PLUGIN_PAGES_ROOT_REGEX\}/, `^${root}`)
 				.replace(/\#\{__PLUGIN_PAGES_ROOT_REGEX_REPLACER\}/, replacer);
 
@@ -45,4 +59,19 @@ export default function routes({
 			};
 		},
 	};
+}
+
+async function readdirRecursive(dir) {
+	const _dirContents = await readdir(dir);
+	const results = [];
+	await Promise.all(
+		_dirContents.map(async (d) => {
+			const withRoot = join(dir, d);
+			const _stat = await stat(withRoot);
+			if (_stat.isDirectory()) {
+				results.push(...(await readdirRecursive(withRoot)));
+			} else results.push(withRoot);
+		}),
+	);
+	return results;
 }
