8bdcd3908371e55c550f07997c24088c5a3d5fc2
reaper <ahoy@barelyhuman.dev>
2025-08-22 23:01:20 +0530

    feat: add example island application with counters and configuration

---
+++ examples/island-example/src/main.jsx
@@ -0,0 +1,9 @@
+import { Counter } from "./counter.jsx";
+import { Counter as Counter2 } from "./another-counter.jsx";
+
+export const Main = () => (
+	<div>
+		<Counter />
+		<Counter2 />
+	</div>
+);
